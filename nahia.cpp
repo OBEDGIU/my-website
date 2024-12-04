f35c14c3874eceab12685332dee5730adabe02be
OBEDGIU <obedigiu.ronald@stud.umu.ac.ug>
2024-12-04 05:18:07 -0800

    Add files via upload

---
+++ nahia.cpp
@@ -0,0 +1,21 @@
+//the sum of two intergers
+#include<iostream>
+using namespace std;
+
+int main(){
+	int num1,num2,sum;
+	//input the first num
+	cout<<"enter first number";
+	cin>>num1;
+	
+	//input the second number
+	cout<<"enter the second number";
+	cin>>num2;
+	
+	//add the intergers
+	sum=num1+num2;
+	//display the result
+	cout<<"the sum is : "<<sum;
+	return 1;
+	
+}
